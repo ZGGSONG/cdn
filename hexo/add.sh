02fcc59d1dff49024f9f7f995961b4946e5373e8
ZGGSONG <zggsong@foxmail.com>
2020-03-09 23:23:34 +0800

    change .sh

--- hexo/add.sh
+++ hexo/add.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 $1
 
-echo -e "\n- $1: https://cdn.jsdelivr.net/gh/ZGGSONG/cdn/hexo/$1" >> README.md
+echo -e "\n- $1: https://cdn.jsdelivr.net/gh/ZGGSONG/cdn@master/hexo/$1" >> README.md
 
